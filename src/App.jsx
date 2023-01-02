37f0b08f4bf6e09ab1481a65b21b4b22cd966d6d
Jonathan Ovalle <jovalle0103@gmail.com>
2023-01-01 19:59:03 -0800

    background

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { PointLightHelper } from "three";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import {
   Backdrop,
+  ContactShadows,
   OrbitControls,
   useHelper,
   softShadows,
@@ -37,50 +38,61 @@ function Lights() {
 
 function App() {
   return (
-    <Canvas camera={{ fov: 40 }} shadows>
-      <ambientLight intensity={0.2} />
-      <Lights />
-      <OrbitControls enableZoom={true} />
-      {/* <Desk position={[0, -5, 0]} scale={[7, 7, 7]} /> */}
-      <Phone
-        position={[3.5, 0, -2]}
-        rotation={[Math.PI / 1.1, 0, -Math.PI]}
-        scale={[0.7, 0.7, 0.7]}
-      />
-      {/* <Mac /> */}
-      {/* <Mac position={[0, 2, 0]} scale={[10, 10, 10]} /> */}
-      <Mac2 position={[0, -1.38, 0]} scale={[10, 10, 10]} castShadow />
-      {/* <Tablet
+    <div className="main">
+      <div className="header">hi</div>
+      <Canvas camera={{ fov: 40 }} shadows className="canvas">
+        <ambientLight intensity={0.2} />
+        <Lights />
+        <OrbitControls enableZoom={true} />
+        {/* <Desk position={[0, -5, 0]} scale={[7, 7, 7]} /> */}
+        <Phone
+          position={[3.5, 0, -2]}
+          rotation={[Math.PI / 1.1, 0, -Math.PI]}
+          scale={[0.7, 0.7, 0.7]}
+        />
+        {/* <Mac /> */}
+        {/* <Mac position={[0, 2, 0]} scale={[10, 10, 10]} /> */}
+        {/* <Mac2 position={[0, -1.38, 0]} scale={[10, 10, 10]} castShadow /> */}
+        <Mac2 position={[0, -0.35, 0]} scale={[10, 10, 10]} castShadow />
+        <ContactShadows
+          position={[0, -0.65, 0]}
+          opacity={0.75}
+          scale={10}
+          blur={1.5}
+          far={0.8}
+        />
+        {/* <Tablet
         position={[0, -0.23, -0.68]}
         rotation={[-0.5, -Math.PI, 0]}
         scale={[10, 10, 10]}
       />
       <Stand position={[-0.6, -1.45, 0]} scale={[0.015, 0.015, 0.015]} /> */}
-      <Tablet
-        position={[-3.4, -0.23, -0.68]}
-        rotation={[-0.5, -Math.PI, 0]}
-        scale={[10, 10, 10]}
-      />
-      <Stand position={[-4, -1.45, 0]} scale={[0.015, 0.015, 0.015]} />
-      {/* <Stars /> */}
-      <Text3D
-        // castShadow
-        curveSegments={32}
-        bevelEnabled
-        bevelSize={0.04}
-        bevelThickness={0.1}
-        height={0.5}
-        lineHeight={0.5}
-        letterSpacing={-0.06}
-        size={1.5}
-        font="/Inter_Bold.json"
-        position={[2, 3, 0]}
-      >
-        henlo
-        <meshNormalMaterial />
-      </Text3D>
-      <Background />
-    </Canvas>
+        <Tablet
+          position={[-3.4, 0.87, -0.68]}
+          rotation={[-0.5, -Math.PI, 0]}
+          scale={[10, 10, 10]}
+        />
+        <Stand position={[-4, -0.35, 0]} scale={[0.015, 0.015, 0.015]} />
+        {/* <Stars /> */}
+        <Text3D
+          // castShadow
+          curveSegments={32}
+          bevelEnabled
+          bevelSize={0.04}
+          bevelThickness={0.1}
+          height={0.5}
+          lineHeight={0.5}
+          letterSpacing={-0.06}
+          size={1.5}
+          font="/Inter_Bold.json"
+          position={[2, 3, 0]}
+        >
+          henlo
+          <meshNormalMaterial />
+        </Text3D>
+        <Background />
+      </Canvas>
+    </div>
   );
 }
 
--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -1,25 +1,42 @@
 import { useRef, useState } from "react";
 import { Backdrop } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
+import { useEffect } from "react";
 
 export default function Background() {
-  const [color, setColor] = useState("#353540");
+  const [color, setColor] = useState("#0580c7");
   const [active, setActive] = useState(false);
   const mesh = useRef();
 
-  useFrame(() => {
-    if (active) {
-      setColor("#ff6013");
-    } else {
-      setColor("#353540");
-    }
-  });
+  const colors = ["#353540", "#04a437", "#ff6013", "#0580c7"];
+  let curColor = -1;
+
+  useEffect(() => {
+    setTimeout(() => {
+      curColor = colors.indexOf(color) + 1;
+      if (curColor >= colors.length) {
+        curColor = 0;
+      }
+      console.log(colors.length);
+      setColor(colors[curColor]);
+    }, 2000);
+  }, [color]);
+
+  // useFrame(() => {
+  //   if (active) {
+  //     setColor("#04a437");
+  //     // setColor("#ff6013");
+  //   } else {
+  //     setColor("#0580c7");
+  //     // setColor("#353540");
+  //   }
+  // });
 
   return (
     <Backdrop
       receiveShadow
       floor={2}
-      position={[0, -1.8, -3]}
+      position={[0, -0.8, -3]}
       scale={[50, 10, 4]}
       onClick={() => {
         setActive(!active);
