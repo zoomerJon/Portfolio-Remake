281443e9cd1b372edbd9d6273cf17740e39b4c90
Jonathan Ovalle <jovalle0103@gmail.com>
2023-01-02 19:13:55 -0800

    text

--- src/WebPage/WebPage.jsx
+++ src/WebPage/WebPage.jsx
@@ -4,7 +4,7 @@ export default function WebPage() {
   return (
     <div className="content-mac">
       <div>
-        <h1>Hello this is a thing</h1>
+        <h1>HELLO THIS IS A TEST UPDATE</h1>
         <h1>hello this is a thing</h1>
         <h1>hello this is a thing</h1>
         <h1>hello this is a thing</h1>
