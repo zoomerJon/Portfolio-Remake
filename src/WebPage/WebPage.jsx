3dd34c23c409f7e16e754d13440dcfa2cfaeeeeb
Jonathan Ovalle <jovalle0103@gmail.com>
2023-02-27 15:07:32 -0800

    links

--- src/WebPage/WebPage.jsx
+++ src/WebPage/WebPage.jsx
@@ -6,15 +6,18 @@ export default function WebPage() {
       <div className="">
         <h4 className="heading">Recent Works/Projects</h4>
         <div className="recent-works">
-          <div className="work-card">
-            <img src="/acfc-demo.png" />
-            <div className="work-title">Megachurch Website</div>
-            <div className="work-tags">
-              <div className="work-tag html">HTML</div>
-              <div className="work-tag css">CSS</div>
-              <div className="work-tag wordpress">Wordpress</div>
+          <a href="https://www.aguilascfc.org/">
+            <div className="work-card">
+              <img src="/acfc-demo.png" />
+              <div className="work-title">Megachurch Website</div>
+              <div className="work-tags">
+                <div className="work-tag html">HTML</div>
+                <div className="work-tag css">CSS</div>
+                <div className="work-tag javascript">JavaScript</div>
+                <div className="work-tag wordpress">Wordpress</div>
+              </div>
             </div>
-          </div>
+          </a>
           <div className="work-card">
             <img src="/old-portfolio-demo.png" />
             <div className="work-title">Previous Portfolio</div>
@@ -53,15 +56,19 @@ export default function WebPage() {
               <div className="work-tag css">CSS</div>
             </div>
           </div>
-          <div className="work-card">
-            <img src="/feedback-app-demo.png" />
-            <div className="work-title">Feedback Web App</div>
-            <div className="work-tags">
-              <div className="work-tag react">React</div>
-              <div className="work-tag javascript">JavaScript</div>
-              <div className="work-tag css">CSS</div>
+          <a href="https://cheerful-empanada-e83c29.netlify.app/">
+            <div className="work-card donuts">
+              <img className="donut" src="/donut-shop.png" />
+              <div className="donuts-background">
+                <div className="work-title">Donut Store</div>
+                <div className="work-tags">
+                  <div className="work-tag react">React</div>
+                  <div className="work-tag javascript">JavaScript</div>
+                  <div className="work-tag css">CSS</div>
+                </div>
+              </div>
             </div>
-          </div>
+          </a>
         </div>
       </div>
     </div>
